48aac9f36062739dddeb0948054ff67a9f15f4b7
Brucejia57 <65301772+Brucejia57@users.noreply.github.com>
2021-07-12 20:34:49 -0400

    Update ece1895assign10.ino

--- ece1895assign10.ino
+++ ece1895assign10.ino
@@ -14,9 +14,9 @@ void loop() {
   if(digitalRead(7)==LOW)
   {
     digitalWrite(9, HIGH); // sets the digital pin 13 on
-    delay(100);            // waits for a second
+    delay(1000);            // waits for a second
     digitalWrite(9, LOW);  // sets the digital pin 13 off
-    delay(100);            // waits for a second
+    delay(1000);            // waits for a second
   }
 
 }
