00f1a48e2a3d40cf85fca12f9807ba12c4c97960
srs190 <stevenschultz99@gmail.com>
2021-08-04 01:12:05 -0400

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -29,37 +29,7 @@ uint16_t currtouched = 0;
 
 
 
-
-
- 
-// define the pins used
-#define CLK 13       // SPI Clock, shared with SD card
-#define MISO 12      // Input data, from VS1053/SD card
-#define MOSI 11      // Output data, to VS1053/SD card
-// Connect CLK, MISO and MOSI to hardware SPI pins. 
-// See http://arduino.cc/en/Reference/SPI "Connections"
-
-// These are the pins used for the breakout example
-#define BREAKOUT_RESET  -1      // VS1053 reset pin (output)
-#define BREAKOUT_CS     -1     // VS1053 chip select pin (output)
-#define BREAKOUT_DCS    8      // VS1053 Data/command select pin (output)
-// These are the pins used for the music maker shield
-#define SHIELD_RESET  -1      // VS1053 reset pin (unused!)
-#define SHIELD_CS     7      // VS1053 chip select pin (output)
-#define SHIELD_DCS    6      // VS1053 Data/command select pin (output)
-#define PIEZO         9
-// These are common pins between breakout and shield
-#define CARDCS 4     // Card chip select pin
-// DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
-#define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
-
-//Adafruit_VS1053_FilePlayer musicPlayer = 
-  // create breakout-example object!
-  //Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
-  // create shield-example object!
- // Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
-
-  
+#define buzzer A1
 #define slidePin 2
 #define spinPin A2 
 
@@ -87,28 +57,6 @@ void setup() {
   //startUpUI();
   //startUpVoice();
 
-//  if (! musicPlayer.begin()) { // initialise the music player
-    // Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
-   //  while (1);
-//  }
-  //Serial.println(F("VS1053 found"));
-  
-   if (!SD.begin(CARDCS)) {
-    Serial.println(F("SD failed, or not present"));
-    while (1);  // don't do anything more
-  }
-    // list files
-  //printDirectory(SD.open("/"), 0);
-  //voice("filename");
-  // Set volume for left, right channels. lower numbers == louder volume!//
- // musicPlayer.setVolume(20,20);
-
-
-
-
-
-
-
 
   if (!cap.begin(0x5A)) {
     Serial.println("MPR121 not found, check wiring?");
@@ -121,8 +69,10 @@ void setup() {
 
   pinMode(slidePin, INPUT);
   pinMode(spinPin, INPUT);
-  pinMode(PIEZO, OUTPUT);
+  pinMode(buzzer, OUTPUT);
   //waitForStartbuttonPress();
+
+  
  piezoStart();
   gameSpeed=100;
   score=0;
